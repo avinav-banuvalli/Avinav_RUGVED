35f7a19c5b89d3e90522c4159527d3f1e0b6265e
Avinav Banuvalli <avinav1006@gmail.com>
2026-08-30 22:30:46 +0530

    Modified the code to handle non-boolean arguments

--- Task_1/q01_triple_and.py
+++ Task_1/q01_triple_and.py
@@ -2,10 +2,12 @@
 
 # Below defined method accepts three boolean arguments and returns the "and" value of the three arguments
 def triple_and(a, b, c):
-    return (a and b and c)
+    return bool(a and b and c)
 
 # Testing the triple_and method with various values
 print(triple_and(True, True, True))
 print(triple_and(False, True, True))
 print(triple_and(False, False, True))
 print(triple_and(False, False, False))
+print(triple_and(1, 2, 3))
+print(triple_and(0, 0, 0))
