d63e32cac9e20013c24287f2f91f66c7ee3807ed
Avinav Banuvalli <avinav1006@gmail.com>
2026-08-30 23:15:55 +0530

    Working version of Sort and Counting char in sorted string

---
+++ Task_1/q02_sort_count.py
@@ -0,0 +1,19 @@
+def sort_count(s):
+    sorted_string = "".join(sorted(s.lower()))
+
+    char_count = {}
+    for char in s.lower():
+        char_count[char] = char_count.get(char, 0) + 1
+
+    return sorted_string, char_count
+
+input_string = input("Enter a string: ")
+sorted_string, char_count = sort_count(input_string)
+
+print("Sorted String: ", sorted_string)
+print("Count of characters in the Sorted String:")
+for char in char_count:
+    if char_count[char] > 1:
+        print(f"{char} appears {char_count[char]} times")
+    else:
+        print(f"{char} appears {char_count[char]} time")
