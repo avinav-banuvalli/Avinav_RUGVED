8b1ed5ea8838b4149edc811a4722e4e4a026fe7c
Avinav Banuvalli <avinav1006@gmail.com>
2026-08-30 22:22:54 +0530

    Added comments

--- Task_1/q01_triple_and.py
+++ Task_1/q01_triple_and.py
@@ -1,6 +1,10 @@
+# Q1-Define a function named “triple_and” that takes three parameters and returns True only if they are all True and False otherwise
+
+# Below defined method accepts three boolean arguments and returns the "and" value of the three arguments
 def triple_and(a, b, c):
     return (a and b and c)
 
+# Testing the triple_and method with various values
 print(triple_and(True, True, True))
 print(triple_and(False, True, True))
 print(triple_and(False, False, True))
