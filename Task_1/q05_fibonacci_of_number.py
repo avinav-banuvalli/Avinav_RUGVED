6c2bb463e808e74ae4e9a4820e38446ea0e851a8
Avinav Banuvalli <avinav1006@gmail.com>
2026-09-01 23:12:13 +0530

    Added comments

--- Task_1/q05_fibonacci_of_number.py
+++ Task_1/q05_fibonacci_of_number.py
@@ -1,3 +1,6 @@
+# Q5-Find the fibonacci of a given number using recursion.
+
+#Function to find the fibonacci number
 def fibonacci(n) :
     if n==1 :
         return 1
@@ -6,12 +9,15 @@ def fibonacci(n) :
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
+# Taking input from user
 num=int(input("Enter a number: "))
 
 if num<0 :
     print("Enter non negative number")
 else :
     print(f"Fibonacci of {num} is {fibonacci(num)}")
+
+
 
         
     
