0758f28b545109f1931459712592937b971ac818
Avinav Banuvalli <avinav1006@gmail.com>
2026-09-02 18:13:56 +0530

    Added comments

--- Task_1/q06_anagrams.py
+++ Task_1/q06_anagrams.py
@@ -1,3 +1,6 @@
+# Q6-Create a function that takes two strings as input and checks whether they are anagrams of each other.
+
+# Method to check if strings are anagram or not
 def anagrams(str1,str2):
     str1 = str1.replace(" ","")
     s1 = str1.lower()
@@ -7,16 +10,18 @@ def anagrams(str1,str2):
     if len(s1) != len(s2):
         return False
 
-    s1=sorted(s1)
-    s2=sorted(s2)
+    s1 = sorted(s1)
+    s2 = sorted(s2)
 
     if s1 == s2:
         return True
     else:
         return False
 
-word1=input("Enter the first string: ")
-word2=input("Enter the second string: ")
+# Taking input from the user
+word1 = input("Enter the first string: ")
+word2 = input("Enter the second string: ")
+
 if (anagrams(word1,word2)):
    print("It is an Anagram")
 else:
