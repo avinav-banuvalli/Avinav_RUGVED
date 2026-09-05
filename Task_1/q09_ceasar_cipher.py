98a5d994dc77caf8a24608c15729afc70b3ec20d
Avinav Banuvalli <avinav1006@gmail.com>
2026-09-05 22:28:06 +0530

    Added comments to certain lines of code

--- Task_1/q09_ceasar_cipher.py
+++ Task_1/q09_ceasar_cipher.py
@@ -1,20 +1,24 @@
-def encrypt(w,n):
-    result=""
+# Q9-Write a python function to encrypt a string using Ceasar's Cipher
+
+def encrypt(w, n):
+    result = ""
     for i in w:
         if i.isalpha():
             if i.islower():
                 position = ord(i) - ord('a')
-                shifted = (position + n)%26
-                new_char=chr(shifted + ord('a'))
+                shifted = (position + n) % 26
+                new_char = chr(shifted + ord('a'))
                 result += new_char
             elif i.isupper():
                 position = ord(i) - ord('A')
-                shifted = (position + n)%26
-                new_char=chr(shifted + ord('A'))
+                shifted = (position + n) % 26
+                new_char = chr(shifted + ord('A'))
                 result += new_char
         else:
             result += i
     return result
-word=input("Enter the word to be encrypted using ceasars cipher:")
-num=int(input("Enter the value of n:"))
-print(encrypt(word,num))
+
+
+word = input("Enter the word to be encrypted using ceasars cipher:")
+num = int(input("Enter the value of n:"))
+print(encrypt(word, num))
