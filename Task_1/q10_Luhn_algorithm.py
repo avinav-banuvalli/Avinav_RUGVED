e022ed2db7ef20bab28eddcef18f75f81d076044
Avinav Banuvalli <avinav1006@gmail.com>
2026-09-06 19:49:38 +0530

    Added comments

--- Task_1/q10_Luhn_algorithm.py
+++ Task_1/q10_Luhn_algorithm.py
@@ -1,14 +1,20 @@
 # Q10-Write a python function to check if a given credit card number is valid or not using Luhn's Algorithm
 
+
 def is_valid(card_number):
+    # removes spaces and dashes so that "4434 5423 5421 3412" works
     digits_str = card_number.replace(" ", "").replace("-", "")
 
+    # a valid number should have only digits
     if not digits_str.isdigit():
         return False
 
     print("Digits_str = ",digits_str)
     digits = [int(d) for d in digits_str]
     print("Digits array",digits)
+
+    
+    # walk the digits from right to left
     total = 0
     for index, digit in enumerate(reversed(digits)):
         if index % 2 == 1:
@@ -19,6 +25,7 @@ def is_valid(card_number):
 
     return total % 10 == 0
 
+# take input from the user and print whether the nummber is valid  
 number = input("Enter the credit card number:")
 if is_valid(number):
     print("The card number is valid")
