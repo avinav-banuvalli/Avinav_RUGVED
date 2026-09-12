aa1aba75f71b5825fa5427eabd4dcc78ffbd2be6
Avinav Banuvalli <avinav1006@gmail.com>
2026-09-12 19:55:11 +0530

    Added comments to each function and some lines of the code

--- Task_3/robot_trajectory_tracking.py
+++ Task_3/robot_trajectory_tracking.py
@@ -1,9 +1,13 @@
+# Discrete Motion Model & Trajectory Tracking of a Robot
+
 import math as math
 import matplotlib.pyplot as plt
 
 # pose is in (x, y, theta) 
 # angle types in degrees
 # start pose = (0, 0, 0)
+
+# Function to calculate pose 
 def compute_pose (pose, name, value):
     x, y, theta = pose
 
@@ -13,12 +17,14 @@ def compute_pose (pose, name, value):
     elif name == "left":
         theta += math.radians(value)
     elif name == "right":
-        theta = theta - math.radians(value)
+        theta -= math.radians(value)
 
     return (x, y, theta)
 
+# Function to run the commands
 def run_commands(commands, start_pose):
     current_pose = start_pose
+    # Creating empty list with start_pose at 0th index
     history = [start_pose]
     for command in commands:
         parts = command.split()
@@ -28,9 +34,9 @@ def run_commands(commands, start_pose):
         current_pose = compute_pose(current_pose, name, value)
         print(f"Initial Pos: {initial_pose} | Executing: {command} | Final Pos: {current_pose}")
         history.append(current_pose)
-    # print(history)
     return history
 
+# Function to plot the points on the graph
 def plot_trajectory(history):
     xs = [pose[0] for pose in history]
     ys = [pose[1] for pose in history]
@@ -38,40 +44,52 @@ def plot_trajectory(history):
     us = [math.cos(theta) for theta in thetas]
     vs = [math.sin(theta) for theta in thetas]
 
+    # Creates a figure and axes in a single call 
     fig, ax = plt.subplots(figsize = (10, 5))
     ax.plot(xs, ys, marker = '.', color = 'black', label = "Path")
     ax.plot(xs[0], ys[0], marker = 'o', color = 'green', label = "Start")
     ax.plot(xs[len(xs) - 1], ys[len(ys) - 1], marker = 'o', color = 'red', label = "End")
+    # Makes small arrows at each (x,y) and at each change in angle
     ax.quiver(xs, ys, us, vs, angles = 'xy')
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
+    # Pad is used to leave some space between heading and graph
     ax.set_title("Robot Trajectory", pad = 20)
+    # To mske the x-axis and y axis equal in dimension
     ax.set_aspect('equal')
-    ax.legend(loc = 'upper left', bbox_to_anchor = (1.02, 1))
+    ax.legend(loc = 'upper left')
+    #To put grids in the graph
     ax.grid(True)
     plt.tight_layout()
     plt.show()
 
+# Function to get input of the number of points
 def get_commands():
     n = int(input("Enter the number of commands:"))
+    # Empty file to store all the commands as strings
     commands = []
     for i in range(n):
         command = input(f"Enter command {i + 1}:")
         commands.append(command)
     return commands 
 
+# Function to read the file
 def read_commands_file():
     filename = input("Enter the command file name:")
     with open(filename) as file:
         lines = file.readlines()
+    # Creating empty list to input values after reading 
     commands = []
     for line in lines:
+        # Strip removes whitespaces, tabs, and \n
         cleaned = line.strip()
         if cleaned:
             commands.append(cleaned)
     return commands
 
+# Giving a choice wheather to type manually or read from file
 choice = input("Type 't' to enter the commands manually, or 'f' to read the commands from a file:")
+# Strip removes whitespaces, tabs, and \n
 if choice == choice.strip().lower() == "t":
     commands = get_commands()
 elif choice == choice.strip().lower() == "f":
@@ -79,6 +97,7 @@ elif choice == choice.strip().lower() == "f":
 else:
     print("Error!!, Enter 't' or 'f'")
 
+# Initializing start position as origin
 start_pose = (0, 0, 0)
 history = run_commands(commands, start_pose)
 plot_trajectory(history)
